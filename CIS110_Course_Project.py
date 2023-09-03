8aede8f97fed1e804edde8d4a11745cf9766d653
username <useremail>
2023-09-03 12:21:45 -0700

    Course Project

--- CIS110_Course_Project.py
+++ CIS110_Course_Project.py
@@ -1,1 +1,17 @@
+print(f"Hello there! You are about to be part of choosing the light or dark! ")
+print(f"Part of the fun is you get to change the character to your liking.")
+print(f"After each answer you will have to hit the enter key to move on.")
+input(f"\nPress the enter key to continue...")
+
+species = input("\nWhat species is your character:  ")
+characterName = input("What is your characters name?  ")
+planetName = input("Which planet are you on?  ")
+movie = input("What is your favorite movie?  ")
+food = input("What is your favorite food?  ")
+
+print(f"\nMay the Force be with you!")
+print(f"\nThere once was a strong {species} named {characterName}. ")
+print(f"\n{characterName} Could not wait to explore {planetName}.  ")
+print(f"\n{characterName} Recieved bad new of a fallen Jedi and has to head to {planetName}.  ")
+print(f"\n{characterName} Rushed to the space port to pick {movie} to watch and {food} to eat on the flight.  ")
 
